6a9fb7274b4534318b832ef71c668f5678ffbb83
ristanovica89 <ristanovica89@gmail.com>
2026-01-31 18:27:20 +0100

    add: WeatherHelper

---
+++ weather-app/app/Http/WeatherHelper.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http;
+
+class WeatherHelper
+{
+
+  public static function getColor($temperature)
+  {
+     $color = 'text-gray-800';
+
+        if($temperature <= 0){
+            $color = 'text-sky-500';
+        }elseif($temperature > 0 && $temperature <= 15){
+            $color = 'text-blue-700';
+        }elseif($temperature > 15 && $temperature <= 25){
+            $color = 'text-green-600';
+        }elseif($temperature > 25){
+            $color = 'text-red-600';
+        }
+
+        return $color;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
