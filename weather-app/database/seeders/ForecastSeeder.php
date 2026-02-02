f6880a533cce3f5a247960e73b1a91ea29e11246
ristanovica89 <ristanovica89@gmail.com>
2026-02-02 01:35:17 +0100

    dz15| ForecastSeeder updated

--- weather-app/database/seeders/ForecastSeeder.php
+++ weather-app/database/seeders/ForecastSeeder.php
@@ -19,9 +19,9 @@ class ForecastSeeder extends Seeder
 
         $city = City::where('name', $cityName)->first();
 
-        if(! $city){
+        if (! $city) {
             $this->command->error('There is no city ' . $cityName . ' on list.');
-            return; 
+            return;
         }
 
         $days = (int) $this->command->ask('How many days do you want to add forecast for?');
@@ -37,15 +37,44 @@ class ForecastSeeder extends Seeder
         for ($i = 1; $i <= $days; $i++) {
             $date = date('Y-m-d', strtotime("+$i day"));
 
-            $description = $faker->randomElement($this->weatherConditions());
+
+            /*
+                ->latest('date')->first();
+                jer $date = date('Y-m-d', strtotime("+$i day")) simulira sledece dane
+             */
+            $day_before = Forecast::where('city_id', $city->id)->latest('date')->first();
+
+            /* 
+                1)Ako vec postoji dan pre, nalazim max temp i dodajem mu od -5 do +5
+                2)Ako prethodnog dana nema, $faker mi pravi temp od -5 do 35 i vec u sl iteraciji
+                ulazimo u 1) 
+             */
+            if ($day_before) {
+                $max_temp = $day_before->max_temp + $faker->numberBetween(-5, 5);
+            } else {
+                $max_temp = $faker->numberBetween(-5, 35);
+            }
+
+            /*
+                Pojednostavljena logika minimalna temperatura je uvek za 12 manja od maksimalne,
+                posto sam poceo sa min i max temp , pa da ne komplikujem za sad sa srednjom temp..
+             */
+            $min_temp = $max_temp - 12;
+
+            /*
+                Vraca $description koji je filtriran zadatim uslovima (zamenjeno 'rainy' > -5)
+            */
+            $description = $faker->randomElement($this->weatherConditions($max_temp));
+
             $probability = null;
-            if($description == 'snowy' || $description == 'rainy'){
-                $probability = $faker->numberBetween(0,100);
+
+            if ($description == 'snowy' || $description == 'rainy') {
+                $probability = $faker->numberBetween(0, 100);
             }
 
             Forecast::create([
-                'min_temp' => $faker->numberBetween(-5, 5),
-                'max_temp' => $faker->numberBetween(15, 35),
+                'min_temp' => $min_temp,
+                'max_temp' => $max_temp,
                 'description' => $description,
                 'probability' => $probability,
                 'date' => $date,
@@ -57,14 +86,20 @@ class ForecastSeeder extends Seeder
 
         $this->command->getOutput()->progressFinish();
         $this->command->info('Forecast successfully added for ' . $days . ' days for city ' . $cityName);
+
+        // Kod mi lici na JS :( 
     }
 
-    private function weatherConditions(){
-        return ['sunny',
-            'cloudy',
-            'windy',
-            'rainy',
-            'snowy',
-            'clear',];
+    private function weatherConditions($temp)
+    {
+        $description = [
+            'sunny' => true,
+            'cloudy' => $temp < 15,
+            'windy' => true,
+            'rainy' => $temp > -5,
+            'snowy' => $temp <= 5,
+            'clear' => true,
+        ];
+        return array_keys(array_filter($description));
     }
 }
--- weather-app/resources/views/forecast.blade.php
+++ weather-app/resources/views/forecast.blade.php
@@ -2,17 +2,51 @@
 @section('title','Weather App')
 
 @section('content')
-@section('banner', 'Forecast - for next 5 days')
+@section('banner', 'Forecast')
 @include('partials.message')
-<div class="flex-1 grid gap-6 p-6
-            grid-cols-1
-            sm:grid-cols-2
-            md:grid-cols-3
-            lg:grid-cols-4
-            justify-items-start">
-@foreach($cityForecasts as $forecast)
-  @include('partials.forecast-card')
-@endforeach
+<div class="mx-auto w-3/4 overflow-x-auto">
+    <table class="min-w-full border border-blue-900 text-blue-900">
+        <thead>
+            <tr class="bg-gray-900 text-white">
+                <th class="px-4 py-2 text-left border border-blue-900">Date</th>
+                <th class="px-4 py-2 text-left border border-blue-900">City</th>
+                <th class="px-4 py-2 text-left border border-blue-900">Max / Min °C</th>
+                <th class="px-4 py-2 text-left border border-blue-900">Weather</th>
+                <th class="px-4 py-2 text-left border border-blue-900">Emoji</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($cityForecasts as $forecast)
+            <tr class="hover:bg-gray-800 transition duration-200">
+                <!-- Datum -->
+                <td class="px-4 py-2 border border-blue-900 text-green-400 font-semibold">
+                    {{ $forecast->date->format('d F') }}
+                </td>
+
+                <!-- Grad -->
+                <td class="px-4 py-2 border border-blue-900 font-bold">
+                    {{ $name }}, {{ $country }}
+                </td>
+
+                <!-- Max / Min -->
+                <td class="px-4 py-2 border border-blue-900 text-green-500 font-bold">
+                    {{ $forecast->max_temp }}° / {{ $forecast->min_temp }}°
+                </td>
+
+                <!-- Description -->
+                <td class="px-4 py-2 border border-blue-900 capitalize">
+                    {{ $forecast->description }}
+                </td>
+
+                <!-- Emoji -->
+                <td class="px-4 py-2 border border-blue-900 text-center text-2xl">
+                    {{ $emojis[$forecast->description] ?? '❓' }}
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
+
 
 @endsection
