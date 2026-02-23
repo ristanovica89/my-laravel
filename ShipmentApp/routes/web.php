26916bbbecf4bf261f4b074ba993dcff7266f2c4
ristanovica89 <ristanovica89@gmail.com>
2026-02-23 22:43:47 +0100

    add: laravel breeze

--- ShipmentApp/routes/web.php
+++ ShipmentApp/routes/web.php
@@ -1,13 +1,21 @@
 <?php
 
-use App\Http\Controllers\HomepageController;
-use App\Http\Controllers\ShipmentController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [HomepageController::class, 'index'])->name('home.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('shipments', ShipmentController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+require __DIR__.'/app.php';
